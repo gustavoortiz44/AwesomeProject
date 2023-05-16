975ef9e25238eb03c82bb019d92685b9c20dd637
gustavoortiz44 <gustavo.ortiz44@gmail.com>
2023-05-16 14:47:22 -0600

    mi primer commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola </Text>
+    </View>
+  )
+}
+
+export default App
